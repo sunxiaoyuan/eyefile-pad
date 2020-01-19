4b1e4abbe8a53a3fa407de8bb72aa84f7c6d59af
sunzhongyuan <sunzhongyuan@syriusrobotics.com>
2020-01-19 23:25:30 +0800

    V20200119

--- app/src/main/java/com/sgeye/exam/android/modules/graph/LineControl.java
+++ app/src/main/java/com/sgeye/exam/android/modules/graph/LineControl.java
@@ -1,11 +1,13 @@
 package com.sgeye.exam.android.modules.graph;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.sgeye.exam.android.R;
@@ -21,147 +23,159 @@ import static android.widget.RelativeLayout.TRUE;
 
 public class LineControl {
 
-	private RelativeLayout mLineRelativeLayout;
-	private ImageView mIndicator;
-	private Context mContext;
-	private List<CommandType> mDirections;
-	private int mCurIndex = 0;
-	private int mOffset = 0;
-	private int mContainerWidthLarge = 0;
-	private int mContainerHeightLarge = 0;
-	private RelativeLayout mImageContainer;
+    private RelativeLayout mLineRelativeLayout;
+    private ImageView mIndicator;
+    private Context mContext;
+    private List<CommandType> mDirections;
+    private int mCurIndex = 0;
+    private int mOffset = 0;
+    private int mContainerWidthLarge = 0;
+    private int mContainerHeightLarge = 0;
+    private RelativeLayout mImageContainer;
+    private LinearLayout mImages;
 
 
-	public LineType getType() {
-		return mType;
-	}
+    public LineType getType() {
+        return mType;
+    }
 
-	private LineType mType;
+    private LineType mType;
 
-	public RelativeLayout getLineRelativeLayout() {
-		return mLineRelativeLayout;
-	}
+    public RelativeLayout getLineRelativeLayout() {
+        return mLineRelativeLayout;
+    }
 
-	public LineControl(Context context, int index) {
-		mContext = context;
+    public LineControl(Context context, int index) {
+        mContext = context;
 
-		mType = LineType.getLineTypeByIndex(index);
+        mType = LineType.getLineTypeByIndex(index);
 
-		// 根据布局文件加载Line
-		int resId = LineType.getResIdByIndex(index);
-		mLineRelativeLayout = (RelativeLayout) LayoutInflater.from(context).inflate(resId, null);
+        // 根据布局文件加载Line
+        int resId = LineType.getResIdByIndex(index);
+        mLineRelativeLayout = (RelativeLayout) LayoutInflater.from(context).inflate(resId, null);
 
-		//组件绘制前监听
-		ViewTreeObserver vto = mLineRelativeLayout.getViewTreeObserver();
-		vto.addOnPreDrawListener(() -> {
-			mContainerHeightLarge = mLineRelativeLayout.getMeasuredHeight();
-			mContainerWidthLarge = mLineRelativeLayout.getMeasuredWidth();
-			return true;
-		});
+        //组件绘制前监听
+        ViewTreeObserver vto = mLineRelativeLayout.getViewTreeObserver();
+        vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                mContainerHeightLarge = mLineRelativeLayout.getMeasuredHeight();
+                mContainerWidthLarge = mLineRelativeLayout.getMeasuredWidth();
 
-		// 获取方向数组（ = ImageView的个数）
-		mDirections = LineType.getCommandTypesByIndex(index);
-		// 获取图片容器 RelativeLayout
-		mImageContainer = (RelativeLayout) mLineRelativeLayout.getChildAt(2);
+                int offsetY = (mContainerHeightLarge - mImages.getMeasuredHeight()) / 2;
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) mImages.getLayoutParams();
+                layoutParams.topMargin = offsetY;
+                mImages.setLayoutParams(layoutParams);
 
-		ViewTreeObserver vtoImageContainer = mImageContainer.getViewTreeObserver();
-		vtoImageContainer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-			@Override
-			public boolean onPreDraw() {
-				int imageContainerWidth = mImageContainer.getMeasuredWidth();
-				mOffset = imageContainerWidth / mDirections.size();
+                mLineRelativeLayout.getViewTreeObserver().removeOnPreDrawListener(this);
+                return false;
+            }
+        });
 
-				// 随机移动
-				nextFirst();
+        // 获取方向数组（ = ImageView的个数）
+        mDirections = LineType.getCommandTypesByIndex(index);
+        // 获取图片容器 RelativeLayout
+        mImageContainer = (RelativeLayout) mLineRelativeLayout.getChildAt(2);
+        mImages = (LinearLayout) mImageContainer.getChildAt(0);
 
-				mImageContainer.getViewTreeObserver().removeOnPreDrawListener(this);
-				return false;
-			}
-		});
+        ViewTreeObserver vtoImageContainer = mImageContainer.getViewTreeObserver();
+        vtoImageContainer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                int imageContainerWidth = mImageContainer.getMeasuredWidth();
+                mOffset = imageContainerWidth / mDirections.size();
 
-		// 获取指示器ImageView
-		mIndicator = (ImageView) mImageContainer.getChildAt(1);
-		// 自身隐藏
-		mLineRelativeLayout.setAlpha(0);
+                // 随机移动
+                nextFirst();
 
-	}
+                mImageContainer.getViewTreeObserver().removeOnPreDrawListener(this);
+                return false;
+            }
+        });
 
-	public void addToParent(ViewGroup view) {
-		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-				ViewGroup.LayoutParams.MATCH_PARENT,
-				ViewGroup.LayoutParams.WRAP_CONTENT);
-		params.addRule(RelativeLayout.CENTER_VERTICAL, TRUE);
-		params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, TRUE);
-		view.addView(mLineRelativeLayout, params);
-	}
+        // 获取指示器ImageView
+        mIndicator = (ImageView) mImageContainer.getChildAt(1);
 
-	public void changeViewBigger(boolean isBigger) {
-		ViewGroup imageContainer = (ViewGroup) mImageContainer.getChildAt(0);
-		for (int i = 0; i < imageContainer.getChildCount(); i++) {
-			View imageView = imageContainer.getChildAt(i);
-			ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
-			if (isBigger) {
-				layoutParams.height = (int) (layoutParams.height * 2);
-			} else {
-				layoutParams.height = (int) (layoutParams.height * 0.5);
-			}
-			imageView.setLayoutParams(layoutParams);
-		}
-	}
+        // 自身隐藏
+        mLineRelativeLayout.setAlpha(0);
+    }
 
-	// 检查指令是否正确
-	public boolean check(CommandType type) {
-		boolean isCorrect = true;
-		// 判断是否正确
-		isCorrect = type == mDirections.get(mCurIndex);
-		// 无论是否正确，都需要移动指示器
-		next();
-		return isCorrect;
-	}
+    public void addToParent(ViewGroup view) {
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.CENTER_VERTICAL, TRUE);
+        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, TRUE);
+        view.addView(mLineRelativeLayout, params);
+    }
 
-	// 移动指示器
-	private void next() {
-		int min = 0;
-		int max = mDirections.size();
-		int newPosition = new Random().nextInt(max - min) + min;
-		int offsetX = (newPosition - mCurIndex) * mOffset;
+    public void changeViewBigger(boolean isBigger) {
+        ViewGroup imageContainer = (ViewGroup) mImageContainer.getChildAt(0);
+        for (int i = 0; i < imageContainer.getChildCount(); i++) {
+            View imageView = imageContainer.getChildAt(i);
+            ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
+            if (isBigger) {
+                layoutParams.height = (int) (layoutParams.height * 2);
+            } else {
+                layoutParams.height = (int) (layoutParams.height * 0.5);
+            }
+            imageView.setLayoutParams(layoutParams);
+        }
+    }
 
-		// 移动指示器位置
-		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) mIndicator.getLayoutParams();
-		layoutParams.leftMargin = mIndicator.getLeft() + offsetX;
-		mIndicator.setLayoutParams(layoutParams);
+    // 检查指令是否正确
+    public boolean check(CommandType type) {
+        boolean isCorrect = true;
+        // 判断是否正确
+        isCorrect = type == mDirections.get(mCurIndex);
+        // 无论是否正确，都需要移动指示器
+        next();
+        return isCorrect;
+    }
 
-		// 更新位置索引
-		mCurIndex = newPosition;
-	}
+    // 移动指示器
+    private void next() {
+        int min = 0;
+        int max = mDirections.size();
+        int newPosition = new Random().nextInt(max - min) + min;
+        int offsetX = (newPosition - mCurIndex) * mOffset;
 
-	// 移动指示器
-	private void nextFirst() {
-		int min = 0;
-		int max = mDirections.size();
-		int newPosition = new Random().nextInt(max - min) + min;
-		int offsetX = (newPosition - mCurIndex) * mOffset;
+        // 移动指示器位置
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) mIndicator.getLayoutParams();
+        layoutParams.leftMargin = mIndicator.getLeft() + offsetX;
+        mIndicator.setLayoutParams(layoutParams);
 
-		// 移动指示器位置
-		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) mIndicator.getLayoutParams();
-		layoutParams.leftMargin =  offsetX + mOffset/2;
-		layoutParams.topMargin = 20;
-		mIndicator.setLayoutParams(layoutParams);
-		mIndicator.setScaleType(ImageView.ScaleType.MATRIX);
+        // 更新位置索引
+        mCurIndex = newPosition;
+    }
 
-		// 更新位置索引
-		mCurIndex = newPosition;
-	}
+    // 移动指示器
+    private void nextFirst() {
+        int min = 0;
+        int max = mDirections.size();
+        int newPosition = new Random().nextInt(max - min) + min;
+        int offsetX = (newPosition - mCurIndex) * mOffset;
+
+        // 移动指示器位置
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) mIndicator.getLayoutParams();
+        layoutParams.leftMargin = offsetX + mOffset / 2;
+        layoutParams.topMargin = 20;
+        mIndicator.setLayoutParams(layoutParams);
+        mIndicator.setScaleType(ImageView.ScaleType.MATRIX);
+
+        // 更新位置索引
+        mCurIndex = newPosition;
+    }
 
 
-	private int getNewPosition(int min, int max, int old) {
-		int newPosition = new Random().nextInt(max - min) + min;
-		if (newPosition == old) {
-			return getNewPosition(min, max, old);
-		} else {
-			return newPosition;
-		}
-	}
+    private int getNewPosition(int min, int max, int old) {
+        int newPosition = new Random().nextInt(max - min) + min;
+        if (newPosition == old) {
+            return getNewPosition(min, max, old);
+        } else {
+            return newPosition;
+        }
+    }
 
 
 }
--- app/src/main/java/com/sgeye/exam/android/modules/graph/LineType.java
+++ app/src/main/java/com/sgeye/exam/android/modules/graph/LineType.java
@@ -10,95 +10,101 @@ import java.util.List;
  */
 
 public enum LineType {
-	LINE_01(0, "0.1", R.layout.delegate_line_01, LineDirectionConstants.DIRECTION_01),
-	LINE_012(1, "0.12", R.layout.delegate_line_012, LineDirectionConstants.DIRECTION_012),
-	LINE_015(2, "0.15", R.layout.delegate_line_015, LineDirectionConstants.DIRECTION_015),
-	LINE_02(3, "0.2", R.layout.delegate_line_02, LineDirectionConstants.DIRECTION_02),
-	LINE_025(4, "0.25", R.layout.delegate_line_025, LineDirectionConstants.DIRECTION_025),
-	LINE_03(5, "0.3", R.layout.delegate_line_03, LineDirectionConstants.DIRECTION_03),
-	LINE_04(6, "0.4", R.layout.delegate_line_04, LineDirectionConstants.DIRECTION_04),
-	LINE_05(7, "0.5", R.layout.delegate_line_05, LineDirectionConstants.DIRECTION_05),
-	LINE_06(8, "0.6", R.layout.delegate_line_06, LineDirectionConstants.DIRECTION_06),
-	LINE_08(9, "0.8", R.layout.delegate_line_08, LineDirectionConstants.DIRECTION_08),
-	LINE_10(10, "1.0", R.layout.delegate_line_10, LineDirectionConstants.DIRECTION_10),
-	LINE_12(11, "1.2", R.layout.delegate_line_12, LineDirectionConstants.DIRECTION_12),
-	LINE_15(12, "1.5", R.layout.delegate_line_15, LineDirectionConstants.DIRECTION_15),
-	LINE_20(13, "2.0", R.layout.delegate_line_20, LineDirectionConstants.DIRECTION_20);
+    LINE_01(0, "0.1", R.layout.delegate_line_01, LineDirectionConstants.DIRECTION_01, 350),
+    LINE_012(1, "0.12", R.layout.delegate_line_012, LineDirectionConstants.DIRECTION_012, 290),
+    LINE_015(2, "0.15", R.layout.delegate_line_015, LineDirectionConstants.DIRECTION_015, 274),
+    LINE_02(3, "0.2", R.layout.delegate_line_02, LineDirectionConstants.DIRECTION_02, 178),
+    LINE_025(4, "0.25", R.layout.delegate_line_025, LineDirectionConstants.DIRECTION_025, 160),
+    LINE_03(5, "0.3", R.layout.delegate_line_03, LineDirectionConstants.DIRECTION_03, 156),
+    LINE_04(6, "0.4", R.layout.delegate_line_04, LineDirectionConstants.DIRECTION_04, 133),
+    LINE_05(7, "0.5", R.layout.delegate_line_05, LineDirectionConstants.DIRECTION_05, 119),
+    LINE_06(8, "0.6", R.layout.delegate_line_06, LineDirectionConstants.DIRECTION_06, 108),
+    LINE_08(9, "0.8", R.layout.delegate_line_08, LineDirectionConstants.DIRECTION_08, 89),
+    LINE_10(10, "1.0", R.layout.delegate_line_10, LineDirectionConstants.DIRECTION_10, 71),
+    LINE_12(11, "1.2", R.layout.delegate_line_12, LineDirectionConstants.DIRECTION_12, 53),
+    LINE_15(12, "1.5", R.layout.delegate_line_15, LineDirectionConstants.DIRECTION_15, 35),
+    LINE_20(13, "2.0", R.layout.delegate_line_20, LineDirectionConstants.DIRECTION_20, 26);
 
-	private final int index;
-	private final String line;
-	private final int ResId;
-	private final List<CommandType> commandTypes;
+    private final int index; // 行数
+    private final String line; // 行标识
+    private final int ResId; // 布局资源id
+    private final List<CommandType> commandTypes; // 方向列表
+    private final int eSize; // E图标大小
 
-	LineType(int index, String line, int resId, List<CommandType> commandTypes) {
-		this.index = index;
-		this.line = line;
-		ResId = resId;
-		this.commandTypes = commandTypes;
-	}
+    LineType(int index, String line, int resId, List<CommandType> commandTypes, int eSize) {
+        this.index = index;
+        this.line = line;
+        ResId = resId;
+        this.commandTypes = commandTypes;
+        this.eSize = eSize;
+    }
 
-	public int getResId() {
-		return ResId;
-	}
+    public int getResId() {
+        return ResId;
+    }
 
-	public int getIndex() {
-		return index;
-	}
+    public int getIndex() {
+        return index;
+    }
 
-	public String getLine() {
-		return line;
-	}
+    public String getLine() {
+        return line;
+    }
 
-	public List<CommandType> getCommandTypes() {
-		return commandTypes;
-	}
+    public List<CommandType> getCommandTypes() {
+        return commandTypes;
+    }
 
-	public static int getIndexByLine(String line) {
-		int index = 0;
-		for (LineType type :
-				LineType.values()) {
-			if (type.getLine().equalsIgnoreCase(line)) {
-				index = type.getIndex();
-			}
-		}
-		return index;
-	}
+    public int geteSize() {
+        return eSize;
+    }
 
-	public static int getResIdByIndex(int index) {
-		int resId = 0;
-		for (LineType type :
-				LineType.values()) {
-			if (type.getIndex() == index) {
-				resId = type.getResId();
-			}
-		}
-		if (resId == 0) {
-			// 如果找不到对应的，就显示0.1的资源
-			return LineType.LINE_01.getResId();
-		}
-		return resId;
-	}
+    public static int getIndexByLine(String line) {
+        int index = 0;
+        for (LineType type :
+                LineType.values()) {
+            if (type.getLine().equalsIgnoreCase(line)) {
+                index = type.getIndex();
+            }
+        }
+        return index;
+    }
 
-	public static LineType getLineTypeByIndex(int index) {
-		LineType lineType = null;
-		for (LineType type :
-				LineType.values()) {
-			if (type.getIndex() == index) {
-				lineType = type;
-			}
-		}
-		return lineType;
-	}
+    public static int getResIdByIndex(int index) {
+        int resId = 0;
+        for (LineType type :
+                LineType.values()) {
+            if (type.getIndex() == index) {
+                resId = type.getResId();
+            }
+        }
+        if (resId == 0) {
+            // 如果找不到对应的，就显示0.1的资源
+            return LineType.LINE_01.getResId();
+        }
+        return resId;
+    }
 
-	public static List<CommandType> getCommandTypesByIndex(int index) {
-		List<CommandType> commandTypes = new ArrayList<>();
-		for (LineType type :
-				LineType.values()) {
-			if (type.getIndex() == index) {
-				commandTypes = type.getCommandTypes();
-			}
-		}
-		return commandTypes;
-	}
+    public static LineType getLineTypeByIndex(int index) {
+        LineType lineType = null;
+        for (LineType type :
+                LineType.values()) {
+            if (type.getIndex() == index) {
+                lineType = type;
+            }
+        }
+        return lineType;
+    }
+
+    public static List<CommandType> getCommandTypesByIndex(int index) {
+        List<CommandType> commandTypes = new ArrayList<>();
+        for (LineType type :
+                LineType.values()) {
+            if (type.getIndex() == index) {
+                commandTypes = type.getCommandTypes();
+            }
+        }
+        return commandTypes;
+    }
 
 }
